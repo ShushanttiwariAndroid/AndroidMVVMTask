59edac302e3dac2540e20eb9da3091cae9cd5958
Shushanttiwari <shushant.tiwari@mobileprogramming.com>
2021-12-01 11:14:22 +0530

    Added test case related to the repositories and viewmodel

--- app/src/test/java/com/shushant/androidmvvmtask/ui/MainViewModelTest.kt
+++ app/src/test/java/com/shushant/androidmvvmtask/ui/MainViewModelTest.kt
@@ -1,17 +1,23 @@
 package com.shushant.androidmvvmtask.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.asLiveData
 import app.cash.turbine.test
 import com.nhaarman.mockitokotlin2.atLeastOnce
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import com.shushant.MainCoroutineRule
+import com.shushant.androidmvvmtask.models.SandboxResponseClassItem
 import com.shushant.androidmvvmtask.network.Api.ACCOUNT_ID
 import com.shushant.androidmvvmtask.network.service.DemoClient
 import com.shushant.androidmvvmtask.network.service.DemoService
 import com.shushant.androidmvvmtask.persistence.DemoDao
 import com.shushant.androidmvvmtask.repository.DemoRepository
 import com.shushant.utils.MockUtil.mockDummyList
+import com.skydoves.sandwich.ApiResponse
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert
 import org.junit.Before
@@ -21,7 +27,7 @@ import kotlin.time.DurationUnit
 import kotlin.time.toDuration
 
 
-class MainViewModelTest{
+class MainViewModelTest {
     private lateinit var viewModel: MainViewModel
     private lateinit var mainRepository: DemoRepository
     private val demoService: DemoService = mock()
@@ -42,19 +48,13 @@ class MainViewModelTest{
         val mockData = mockDummyList()
         whenever(demoDao.getData()).thenReturn(mockData)
 
-        val fetchedDataFlow = mainRepository.getData(
-            onStart = {},
-            onSuccess = {},
-            onError = {}
-        ).test(2.toDuration(DurationUnit.SECONDS)) {
-            val item = awaitItem()
-            Assert.assertEquals(item[0].accountId, ACCOUNT_ID)
-            awaitComplete()
-        }
+        val observer: Observer<List<SandboxResponseClassItem>?> = mock()
+        val fetchedData: LiveData<List<SandboxResponseClassItem>> =
+            mainRepository.getData(onSuccess = {}, onError = {}, onStart = {}).asLiveData()
+        fetchedData.observeForever(observer)
+        delay(500L)
         verify(demoDao, atLeastOnce()).getData()
-
-        fetchedDataFlow.apply {
-            // runBlocking should return Unit
-        }
+        verify(observer).onChanged(mockData)
+        fetchedData.removeObserver(observer)
     }
 }
